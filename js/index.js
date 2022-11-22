bd8ac525d3fb9614f2d67665c0f3dd6c9a3e79a4
Diogo Mesquita <doc.diogomesquita@gmail.com>
2022-11-22 15:27:10 -0300

    ta indo

--- js/index.js
+++ js/index.js
@@ -1,11 +1,14 @@
 let campoTexto = document.querySelector('#campo-texto');
 let butao = document.querySelector('.btn-add');
 let lista = document.querySelector('.lista');
+let listado = [];
+// let contador = 0;
 
 function addNewItem() {
     let valorCampo = campoTexto.value;
 
-    let novoItem = `<div class="item">
+    if((valorCampo !== "") && (valorCampo !== null) && (valorCampo !== undefined)){
+        let novoItem = `<div class="item">
     <div class="item-icone">
         <i class="mdi mdi-checkbox-blank"></i>
     </div>
@@ -13,21 +16,34 @@ function addNewItem() {
         ${valorCampo}
     </div>
     <div class="item-botao">
-        <button onclick="deletar()" class="delete"> <i class="mdi mdi-trash-can-outline"></i> Deletar</button>
+        <button onclick="deletaItem()" class="delete"> <i class="mdi mdi-trash-can-outline"></i> Deletar</button>
     </div>
 </div>`;
     lista.innerHTML += novoItem;
+
+    /********* */
+    // contador++;
+    // localStorage.setItem(`${contador}`, `${valorCampo}`)
+
+    listado.push(valorCampo);
+    localStorage.setItem("listado", JSON.stringify(listado));
+    /******** */
+    }
+    
 
     campoTexto.value = "";
     campoTexto.focus();
 }
 
+function deletaItem() {
 
-//REMOVER ANTES DE ENTREGAR!!!!!!!!
-// campoTexto.addEventListener("keyup", function(event){
-// //a tecla enter tem valor 13
-//     if(event.keyCode === 13){
-//         event.preventDefault;
-//         butao.click();
-//     }
-// })
+}
+
+// REMOVER ANTES DE ENTREGAR!!!!!!!!
+campoTexto.addEventListener("keyup", function(event){
+//a tecla enter tem valor 13
+    if(event.keyCode === 13){
+        event.preventDefault;
+        butao.click();
+    }
+})
